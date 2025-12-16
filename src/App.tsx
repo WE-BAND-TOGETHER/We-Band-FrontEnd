cb3e722ed9f627029db0bf9ad2fedf5fd8cfa604
jin <qktjwl123@gmail.com>
2025-12-16 22:35:40 +0900

    feat: 캘린더 api 연동

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import ProtectedRoute from '@components/ProtectedRoute';
 
 import OnBoarding from '@pages/Onboarding';
 import Auth from '@pages/Auth';
-import MyCalenderPage from '@pages/MyCalendar';
+import MyCalender from '@pages/MyCalendar';
 import Meet from '@pages/Meet';
 import MyPage from '@pages/MyPage';
 import MeetDetail from '@pages/MeetDetail';
@@ -21,7 +21,7 @@ function App() {
 
           <Route element={<ProtectedRoute />}>
             <Route element={<Layout />}>
-              <Route index element={<MyCalenderPage />} />
+              <Route index element={<MyCalender />} />
               <Route path="/meet" element={<Meet />} />
               <Route path="/meet/:meetId" element={<MeetDetail />} />
               <Route path="/mypage" element={<MyPage />} />
